4693767b6de2859524fbfe570d6e3a2ed373baa3
sethcarrino <sethcarrino@gmail.com>
2018-07-11 08:31:34 -0400

    Convert to MVC design pattern

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Seth Carrino on 7/11/18.
+//  Copyright © 2018 Seth Carrino. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
